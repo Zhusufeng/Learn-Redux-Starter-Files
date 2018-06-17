e6431a91472cc95351a4730b21a81d87db9a6184
Lisa Gee <zhusufeng@gmail.com>
2018-06-17 16:57:33 -0700

    Combine reducers into 1 rootReducer

---
+++ learn-redux/client/reducers/index.js
@@ -0,0 +1,14 @@
+// You can only use 1 reducer in Redux
+// So you put all your reducers together in rootReducer
+
+import { combineReducers } from 'redux';
+import { routerReducer } from 'react-router-redux';
+
+import posts from './posts';
+import comments from './comments';
+
+// 3 things live in state: posts, comments, routes
+// Create the rootReducer with those items:
+const rootReducer = combineReducers({posts, comments, router: routerReducer });
+
+export default rootReducer;
