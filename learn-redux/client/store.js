86b4f154eba16baabe910de2d8b0a1f190bbbc07
Lisa Gee <zhusufeng@gmail.com>
2018-06-17 16:14:34 -0700

    Fix typo

--- learn-redux/client/store.js
+++ learn-redux/client/store.js
@@ -1,5 +1,5 @@
 import { createStore, compose } from 'redux';
-import { syncHistoryithStore } from 'react-router-redux';
+import { syncHistoryWithStore } from 'react-router-redux';
 import { browserHistory } from 'react-router';
 
 // import root reducer
