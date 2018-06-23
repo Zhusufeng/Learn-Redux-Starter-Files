b363b36bff9bed33fbf09a59705701a6bcdf89d7
Lisa Gee <zhusufeng@gmail.com>
2018-06-23 14:30:07 -0700

    Fill out inner reducer postComments to remove a comment from state

--- learn-redux/client/reducers/comments.js
+++ learn-redux/client/reducers/comments.js
@@ -8,8 +8,38 @@
 // 2. copy of current state
 // Reudcer takes this in and returns an updated copy of store
 
+// This will update individual comments
+function postComments(state = [], action) {
+  switch(action.type) {
+    case 'ADD_COMMENT':
+      // return existing state with new comment
+      return [...state, {
+        user: action.author,
+        text: action.comment
+      }];
+    case 'REMOVE_COMMENT':
+      console.log('Removing a comment')
+      // return new state W/O the deleted comment
+      return [
+        // from the state of array to the one we want to delete
+        ...state.slice(0, action.i), 
+        // after the deleted one to the end
+        ...state.slice(action.i + 1)
+      ];
+    default:
+      return state;
+  }
+}
+
 function comments(state = [], action) {
-  console.log(state, action);
+  if (typeof action.postId !== 'undefined') {
+    return {
+      // take the current state
+      ...state, 
+      // overwrite this post with a new one
+      [action.postId]: postComments(state[action.postId], action)
+    } 
+  }
   return state;
 }
 
