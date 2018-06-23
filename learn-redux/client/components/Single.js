c7d68bae08425c466995524b25e295f6aa1b0c39
Lisa Gee <zhusufeng@gmail.com>
2018-06-23 13:29:21 -0700

    Create Single (Photo) component

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
 const Single = React.createClass({
   render() {
+    // get index of the post (get the url, which is passed down from params from react router)
+    const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+    
+    // the index gets us the post
+    const post = this.props.posts[i];
+    
     return (
       <div className="single-photo">
-        I'm the single
+        <Photo i={i} post={post} {...this.props} />
+        <Comments />
       </div>
     );
   }
