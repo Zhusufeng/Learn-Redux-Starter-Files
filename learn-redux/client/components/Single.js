3cab770664fa136f5f0fdf27ab20edb2d2a3191c
Lisa Gee <zhusufeng@gmail.com>
2018-06-23 13:50:37 -0700

    Pass down props from Single to Comments

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -4,16 +4,21 @@ import Comments from './Comments';
 
 const Single = React.createClass({
   render() {
+    const {postId} = this.props.params;
+
     // get index of the post (get the url, which is passed down from params from react router)
-    const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+    const i = this.props.posts.findIndex((post) => post.code === postId);
     
     // the index gets us the post
     const post = this.props.posts[i];
     
+    // get comments
+    const postComments = this.props.comments[postId] || [];
+
     return (
       <div className="single-photo">
         <Photo i={i} post={post} {...this.props} />
-        <Comments />
+        <Comments postComments={postComments} />
       </div>
     );
   }
